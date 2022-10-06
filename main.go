e594bf4d493d502cf77cc733d6926530f43588d3
Henrique Nery <henriquenery07@gmail.com>
2022-10-06 14:10:38 -0300

    feat: add GETHandler function

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"net/http"
 	"fmt"
 	"database/sql"
+	"encoding/json"
 
 	_ "github.com/lib/pq"
 )
+
+type Person struct {
+	Name     string `json:"name"`
+	Nickname string `json:"nickname"`
+}
 
 const (
 	host     = "172.17.0.2"
@@ -35,9 +41,33 @@ func OpenConnection() *sql.DB {
 	return db
 }
 
+func GETHandler(w http.ResponseWriter, r *http.Request) {
+	db := OpenConnection()
+
+	rows, err := db.Query("SELECT * FROM person")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var people []Person
+
+	for rows.Next() {
+		var person Person
+		rows.Scan(&person.Name, &person.Nickname)
+		people = append(people, person)
+	}
+
+	peopleBytes, _ := json.MarshalIndent(people, "", "\t")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(peopleBytes)
+
+	defer rows.Close()
+	defer db.Close()
+}
+
 func main() {
 	http.HandleFunc("/", GETHandler)
 	http.HandleFunc("/inset", POSTHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
